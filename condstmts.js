c91e8726a09d065009e0ced47b1ea16ffa7fef49
Hema-mala16 <68274965+Hema-mala16@users.noreply.github.com>
2020-07-22 20:08:08 +0530

    Add files via upload

---
+++ condstmts.js
@@ -0,0 +1,32 @@
+//while//
+var x=1;
+while(x < 10){
+    console.log(x);
+    x++;
+}
+//switch statement//
+
+let x="fruits"
+ switch(x) {
+    case x: 
+    
+        console.log(`hello`);
+        break;
+        case y:
+       console.log(`welcome`);
+        break;
+        default:
+            console.log(`better luck next nxt Time`);
+      }
+
+
+//do -while//
+let i=0;
+        do{
+            console.log(`The number is `+ i);
+            i++;
+
+        }
+        while(i<5);
+           
+        
